79613659893bc0c7940c9796ab94085629df90cf
mr.Shu <mr@shu.io>
2014-02-05 23:46:44 +0100

    default todo

--- todotxt.rs
+++ todotxt.rs
@@ -10,9 +10,12 @@ struct Todo {
 fn create_task(todo: ~str, id: int) -> Todo {
         let task = Todo{id: id, raw_todo: todo, todo: ~"", priority: '^'};
 
+        task.todo = todo;
+
         return task;
 }
 
 fn main () {
 
+        let t = create_task(~"some important task", 1);
 }
