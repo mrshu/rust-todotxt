cd7902e83bd28d7fd430295bae59d3693b37ef17
mr.Shu <mr@shu.io>
2014-02-05 23:43:54 +0100

    a few more updates

--- todotxt.rs
+++ todotxt.rs
@@ -3,8 +3,14 @@
 struct Todo {
         id: int,
         todo: ~str,
-        priority: int,
+        priority: char,
         raw_todo: ~str
+}
+
+fn create_task(todo: ~str, id: int) -> Todo {
+        let task = Todo{id: id, raw_todo: todo, todo: ~"", priority: '^'};
+
+        return task;
 }
 
 fn main () {
