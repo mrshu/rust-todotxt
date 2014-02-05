de2a771718baf59166692c45fb07cf10236b05e5
mr.Shu <mr@shu.io>
2014-02-06 00:17:48 +0100

    finished

--- todotxt.rs
+++ todotxt.rs
@@ -4,18 +4,20 @@ struct Todo {
         id: int,
         todo: ~str,
         priority: char,
+        finished: bool,
         raw_todo: ~str,
         contexts: & 'static [~str],
         projects: & 'static [~str]
 }
 
 fn create_task(todo: ~str, id: int) -> Todo {
-        let mut task = Todo{id: id, raw_todo: todo.clone(), todo: ~"", priority: '^',
-                                contexts: &[], projects: &[]};
+        let mut task = Todo{id: id, raw_todo: todo.clone(), todo: ~"", finished: false, 
+                                priority: '^', contexts: &[], projects: &[]};
 
         if (todo[0] == "x".as_bytes()[0]) {
-                println("finished");
+                task.finished = true;
         }
+
         task.todo = todo.clone();
 
         return task;
